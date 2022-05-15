be51a3847f7dbe35ef921ed693afaf1c99eaa3d5
David Belisle <david1albertogmail.com>
2022-05-14 21:09:00 -0600

    local to github

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ppt_converter
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = " All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                }
+            }
+
+            directory.Text = filePath;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (directory.Text != "")
+            {
+                converter myConverter = new converter();
+                myConverter.toPng(filePath);
+            }
+        }
+    }
+}
---
+++ converter.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Core;
+using System.IO;
+
+namespace ppt_converter
+{
+    class converter
+    {
+        public void toPng(string inputFile)
+        {
+            Presentation pptPresentation = null;
+            try
+            {
+               
+
+                Application Application = new Application();
+                pptPresentation = Application.Presentations
+                .Open(inputFile, MsoTriState.msoFalse, MsoTriState.msoFalse
+                , MsoTriState.msoFalse);
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+            String outputDirectory = Directory.GetCurrentDirectory() + "\\output";
+            createDirectory(outputDirectory);
+            int counter = 1;
+            foreach (_Slide slide in pptPresentation.Slides)
+            {
+                string slideName = outputDirectory + "\\slide" + counter + ".png";
+                slide.Export(slideName, "png", 1920, 1080);
+                counter++;
+            }
+           
+
+
+        }
+
+        void createDirectory(String path)
+        {
+            
+            try
+            {
+               
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            
+        }
+    }
+}
